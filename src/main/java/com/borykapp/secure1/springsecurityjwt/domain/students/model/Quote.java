25c12cd5751190b3e5a8a3eed996f61e88c8e96c
pboryka <pboryka@future-processing.com>
2020-01-02 13:55:13 +0100

    Implemented rest template pattern, added tests, fixed some part of code

---
+++ src/main/java/com/borykapp/secure1/springsecurityjwt/domain/students/model/Quote.java
@@ -0,0 +1,18 @@
+package com.borykapp.secure1.springsecurityjwt.domain.students.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Quote {
+    private String type;
+    private Value value;
+    private Student student;
+}
