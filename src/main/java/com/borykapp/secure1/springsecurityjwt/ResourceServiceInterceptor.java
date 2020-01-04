77bd6840181bba69ca34429060b31c52a7fdc217
pboryka <pboryka@future-processing.com>
2020-01-04 13:42:01 +0100

    Added comment

--- src/main/java/com/borykapp/secure1/springsecurityjwt/ResourceServiceInterceptor.java
+++ src/main/java/com/borykapp/secure1/springsecurityjwt/ResourceServiceInterceptor.java
@@ -6,10 +6,17 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+This is an example interceptor, there are 3 method that can be overridden
+per, pos, and afterCompletion. 
+ */
+
 @Component
 public class ResourceServiceInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return true;
     }
+
+
 }
